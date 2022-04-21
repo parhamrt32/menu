116f8ed20e3f0c6d6adca563a765d8b75d9bdb51
parhamrt32 <92752702+parhamrt32@users.noreply.github.com>
2022-04-21 10:52:54 -0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="title">
+        <h3> Our Menu </h3>
+        <div className="underline"> </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
